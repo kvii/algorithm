abaa045e0ae37574b252a6001ab4a8f812590e16
kvii2002 <13721952202@163.com>
2021-02-16 15:37:16 +0800

    import testify

--- find-max-subarray_test.go
+++ find-max-subarray_test.go
@@ -5,19 +5,16 @@ import (
 	"math"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestFindMaxSubarray(t *testing.T) {
 	A := []int{13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7}
 	ans := MaxSubarray{7, 10, 43}
 
-	if info1 := FindMaxSubarray(A, 0, len(A)-1); info1 != ans {
-		t.Fatal("info1 failed", info1)
-	}
-
-	if info2 := FindMaxSubarray2(A); info2 != ans {
-		t.Fatal("info2 failed", info2)
-	}
+	assert.Equal(t, FindMaxSubarray(A, 0, len(A)-1), ans)
+	assert.Equal(t, FindMaxSubarray2(A), ans)
 }
 
 func Test413(t *testing.T) {
